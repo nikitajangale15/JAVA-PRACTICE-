c25f747b1d358d2bea5de777dff621d5e5e9965d
nikitajangale15 <109540643+nikitajangale15@users.noreply.github.com>
2022-07-19 14:08:28 +0530

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,3 +1,5 @@
+// Given an integer num input the objective is to write a code to Check if a Given Number is Positive or Negative in Java Language.
+
 import java.util.Scanner;
 public class Number
 {
