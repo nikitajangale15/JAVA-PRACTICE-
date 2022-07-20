166b7f08e93e69ff9466753befe45d7ff282e834
nikitajangale15 <109540643+nikitajangale15@users.noreply.github.com>
2022-07-20 17:23:49 +0530

    commit

---
+++ number4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class number4 {
+    public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+    int num1 =sc.nextInt();
+    int num2 = sc.nextInt();
+    int num3 = sc.nextInt();
+    //checking if num1 is greatest
+    if (num1 >= num2 && num1 >= num3)
+        System.out.println (num1 + " is the greatest");
+
+    //checking if num2 is greatest
+    else if (num2 >= num1 && num2 >= num3)
+        System.out.println (num2 + " is the greatest");
+
+    //checking if num2 is greatest
+    else if (num3 >= num1 && num3 >= num2)
+        System.out.println (num3 + " is the greatest");
+  }
+}
