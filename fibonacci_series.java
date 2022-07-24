63109d833781adff022e22efc52a58daf3906623
nikitajangale15 <109540643+nikitajangale15@users.noreply.github.com>
2022-07-24 23:14:14 +0530

    commit

---
+++ fibonacci_series.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fibonacci_series {
+    public static void main(String [] s) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int a=0, b=1;
+        System.out.print1( a + " , " + b + "  , ");
+        int nextTerm;
+        for ( int i=2; i< num; i++)
+        {
+            nextTerm = a + b;
+            a=b;
+            b= nextTerm;
+            System.out.print(nextTerm+ " , ");
+        }
+    }
+}
