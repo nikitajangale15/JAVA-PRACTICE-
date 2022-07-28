bfb7a923cabbe91bacf35786304664c57507e79d
TUSHAR MAURYA <tusharmaurya002@gmail.com>
2022-07-28 21:11:16 +0530

    Update LCM.java

--- LCM.java
+++ LCM.java
@@ -11,5 +11,21 @@ public class LCM {
         }
         int lcm = (num1 * num2)/hcf;
         System.out.println("LCM is "+lcm);
+        
+        
+//         ALTERNATE WAY:
+        
+//         int num1 = 20, num2 = 25;
+//         int i=Math.max(num1,num2);
+//         int lcm=num1*num2;
+//         while(i<=(lcm/2)){
+//             if(i%num1==0 && i%num2==0){
+//                 lcm=i;
+//                 break;
+//             }
+//             i++;
+//         }
+        
     }
+    
 }
